1462e564232af58e2e801ac410669177c5eed68f
Marcelo Mastroiani <mastroianimarcelo04@gmail.com>
2025-06-13 17:23:30 -0300

    feat: implement CreateProductDto and update ProductsController

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   Delete,
   Inject,
+  ParseIntPipe,
 } from '@nestjs/common';
 
 import { PRODUCT_SERVICE } from '../config/service';
@@ -35,13 +36,15 @@ export class ProductsController {
   }
 
   @Get(':id')
-  //TODO: Corregir el tipo de retorno a number
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id') id: number) {
     return this.productsClient.send({ cmd: 'find_product' }, { id });
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateProductDto: UpdateProductDto,
+  ) {
     // return this.productsService.update(+id, updateProductDto);
     return this.productsClient.send(
       { cmd: 'update_product' },
@@ -50,8 +53,8 @@ export class ProductsController {
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id') id: number) {
     // return this.productsService.remove(+id);
-    return `This action removes a #${id} product`;
+    return this.productsClient.send({ cmd: 'remove_product' }, { id });
   }
 }
