ecdd5e42214d3bc0ce4e5294ae562898fc3b4398
Marcelo Mastroiani <mastroianimarcelo04@gmail.com>
2025-10-15 17:13:49 -0300

    Add: auth-ms DTO

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,31 @@
-import { Controller, Get, Inject, Post } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { NATS_SERVICE } from 'src/config';
+import { RegisterUserDto } from './dto/register-user.dto';
+import { catchError } from 'rxjs';
+import { error } from 'console';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
 
   @Post('register')
-  registeUser() {
-    return this.client.send('auth.register.user', {});
+  registeUser(@Body() registerUserDto: RegisterUserDto) {
+    return this.client.send('auth.register.user', registerUserDto).pipe(
+      catchError((error) => {
+        throw new Error(error);
+      }),
+    );
   }
 
   @Post('login')
-  loginUser() {
-    return this.client.send('auth.login.user', {});
+  loginUser(@Body() loginUserDto: LoginUserDto) {
+    return this.client.send('auth.login.user', loginUserDto).pipe(
+      catchError((error) => {
+        throw new Error(error);
+      }),
+    );
   }
 
   @Get('verify')
